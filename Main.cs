7b8cec61c7e27e8e097975f3faf1f9de3acebbb0
ARAYE Anthony <anthonynjiva@free.fr>
2018-04-07 15:14:28 +0200

    test for student

---
+++ Main.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace trader_game
+{
+    class Main{
+        static void Main(string[] args){
+            Game o = new Game();
+            System.Console.WriteLine(Game.purchase_init);
+        }
+    }
+}
